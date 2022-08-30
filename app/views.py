9ff37784cf98cd7f039d812913bdc326d1111447
atienoobwanda <atienoobwanda@gmail.com>
2022-08-30 08:16:43 +0300

    adding a view for rendering products.html

--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def product(request):
+    return render(request,'product.html')
