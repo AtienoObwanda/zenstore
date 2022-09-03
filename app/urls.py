95c49deb284af4c21588fdd23ddf281880f277a0
atienoobwanda <atienoobwanda@gmail.com>
2022-09-03 22:46:21 +0300

    adding a url for the landing view

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('watch', views.product, name = 'product'),
+    path('watch/', views.product, name = 'product'),
+    path('home/', views.landing, name = 'landing'),
 ]
