77510f5b76e6cc0d29c5770046d1e8799d2b249d
atienoobwanda <atienoobwanda@gmail.com>
2022-09-08 19:13:17 +0300

    navbar css

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 path('shipping/', views.shipping, name='shipping'),
-path('cart', views.cart, name='cart'),
-path('payment', views.payment, name='payment'),
-path('profile', views.profile, name='profile'),
-path('favorites', views.favorites, name='favorites'),
-path('edit/profile', views.edit_profile, name='edit_profile'),
+path('cart/', views.cart, name='cart'),
+path('payment/', views.payment, name='payment'),
+path('profile/', views.profile, name='profile'),
+path('favorites/', views.favorites, name='favorites'),
+path('edit/profile/', views.edit_profile, name='edit_profile'),
 ]
