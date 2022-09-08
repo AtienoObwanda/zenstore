3ebcefacad76144384f73734f48cef37c6dc8b60
atienoobwanda <atienoobwanda@gmail.com>
2022-09-08 19:00:48 +0300

    adding user related views and urls

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('shipping/'),
+    # path('cart'),
+    # path('payment'),
+    # path('profile'),
+    # path('favorites'),
+    # path('edit/profile'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def shipping(request):
+    return render(request,'shipping.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def payment(request):
+    return render(request,'payment.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def favorites(request):
+    return render(request,'favorites.html')
+
+def edit_profile(request):
+    return render(request,'edit_profile.html')
