14a4c170fac8a18ffc4407aadf4472889e0f5cd5
atienoobwanda <atienoobwanda@gmail.com>
2022-09-08 19:07:30 +0300

    adding view-specific urls

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shipping/'),
-    # path('cart'),
-    # path('payment'),
-    # path('profile'),
-    # path('favorites'),
-    # path('edit/profile'),
+path('shipping/', views.shipping, name='shipping'),
+path('cart', views.cart, name='cart'),
+path('payment', views.payment, name='payment'),
+path('profile', views.profile, name='profile'),
+path('favorites', views.favorites, name='favorites'),
+path('edit/profile', views.edit_profile, name='edit_profile'),
 ]
